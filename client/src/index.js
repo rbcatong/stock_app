8c3da3e05a93cdd2890ef4de3d69c8c9af121d26
danny <danny@Dannys-MacBook-Pro.local>
2020-12-13 20:12:02 -0500

    added react and added concurrently

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import {Provider} from 'react-redux'
+// import {createStore , applyMiddleware , compose} from 'redux'
+// import reduxThunk from 'redux-thunk'
+
+import App from './components/App.js'
+// import reducers from './reducers'
+
+// import axios from 'axios'
+// window.axios = axios
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const store = createStore( reducers, composeEnhancers(applyMiddleware(reduxThunk)))
+
+
+// ReactDOM.render(
+// <Provider store={store}>
+//     <App/>
+// </Provider>
+//     , document.querySelector('#root') )
+
+
+    ReactDOM.render( <App/> , document.querySelector('#root') )
--- index.js
+++ index.js
@@ -7,9 +7,24 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 
-app.get('/' ,  (req,res)=>{
-    res.send('start')
-})
+
+      // this makes sure express behaves correctly
+      if (process.env.NODE_ENV === 'production') {
+        // Express will serve up production assets
+        // like our main.js file, or main.css file!
+        app.use(express.static('client/build'));
+
+        // Express will serve up the index.html file
+        // if it doesn't recognize the route
+         //express will serve up the index.html if it doesnt recognize the route
+
+
+
+        app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+      }
+
 
 
 const PORT = process.env.PORT || 5000
